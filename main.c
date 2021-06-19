88d4f577276d229ea883b9ad5e2929e976f06b18
Jacob Corbin <jacobwcorbin@gmail.com>
2021-06-19 15:27:30 -0700

    Initial project version

---
+++ main.c
@@ -0,0 +1,44 @@
+// Documentation Section 
+// main.c
+
+// Runs on TM4C123 LaunchPad
+// Input From: 
+// Output to:
+//
+// 
+// Author: Enter your name here
+// Date: 
+// Last modified: 
+
+// 1. Pre-processor Directives Section
+// Constant declarations to access port registers using 
+// symbolic names instead of addresses
+// include directives
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+
+// 2. Declarations Section
+//   Global Variables
+//   Insert Function Prototypes here
+
+
+
+
+// 3. Subroutines Section
+// MAIN: Mandatory for a C Program to be executable
+
+int main(void){
+// Declare variables here
+// Initialize the ports here
+	
+  while(1){
+// Insert your code here
+  }
+}
+
+
+// Insert subroutines here 
+// Inputs: 
+// Outputs:
+// Notes of functionality 
+
